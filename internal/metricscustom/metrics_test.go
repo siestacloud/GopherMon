638dabc2c1ad85ba5da86eff80ecf41de13ff983
siesta.cloud@gmail.com <siesta.cloud@gmail.com>
2022-04-20 22:36:17 +0300

    add test

---
+++ internal/metricscustom/metrics_test.go
@@ -0,0 +1,75 @@
+package metricscustom
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMetricPool(t *testing.T) {
+	tmp := MetricsPool{M: map[string]Metric{}}
+	test := []struct {
+		name string
+		want MetricsPool
+	}{
+		{
+			name: "Test #1",
+			want: tmp,
+		},
+	}
+	for _, tt := range test {
+		mp := NewMetricsPool()
+		assert.Equal(t, tt.want, *mp)
+	}
+}
+
+func TestNewMetrics(t *testing.T) {
+
+	test := []struct {
+		name   string
+		values []string
+		want   Metric
+	}{
+		{
+			name:   "Test #1",
+			values: []string{"testMetric", "counter", "1"},
+			want:   Metric{Name: "testMetric", Value: 1, Types: "counter"},
+		},
+		{
+			name:   "Test #2",
+			values: []string{"metric2", "gouge", "123"},
+			want:   Metric{Name: "metric2", Value: 123, Types: "gouge"},
+		},
+		{
+			name:   "Test #3",
+			values: []string{"metrics3", "counter", "111"},
+			want:   Metric{Name: "metrics3", Value: 111, Types: "counter"},
+		},
+	}
+	for _, tt := range test {
+		m, err := NewMetric(tt.values[1], tt.values[0], tt.values[2])
+		if err != nil {
+			fmt.Println(err)
+		}
+		assert.Equal(t, tt.want, *m)
+	}
+}
+
+func TestConvert(t *testing.T) {
+	tmp := MetricsPool{M: map[string]Metric{}}
+	test := []struct {
+		name string
+
+		want MetricsPool
+	}{
+		{
+			name: "Test #1",
+			want: tmp,
+		},
+	}
+	for _, tt := range test {
+		mp := NewMetricsPool()
+		assert.Equal(t, tt.want, *mp)
+	}
+}
