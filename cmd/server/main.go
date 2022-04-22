f3b83bb601156957c14c113d9061e10640b9d7b2
siesta.cloud@gmail.com <siesta.cloud@gmail.com>
2022-04-22 21:33:53 +0300

    add

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/siestacloud/service-monitoring/internal/server"
 	"github.com/siestacloud/service-monitoring/internal/server/config"
@@ -20,7 +21,7 @@ func main() {
 	}
 	s := server.New(&cfg)
 	if err = s.Start(); err != nil {
-		return
+		os.Exit(0)
 	}
-
+	os.Exit(0)
 }
