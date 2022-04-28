fe5a096401c6f2b38c0bf39fc7d62f62a033f510
siesta.cloud@gmail.com <siesta.cloud@gmail.com>
2022-04-28 07:30:19 +0300

    f

--- internal/server/server.go
+++ internal/server/server.go
@@ -104,6 +104,7 @@ func (s *APIServer) configureLogger() error {
 //configureRouter Set handlers for URL path's
 func (s *APIServer) configureEchoRouter() {
 	s.e.POST("/update/:type/:name/:value", s.UpdateParam())
+	s.e.POST("/update", s.UpdateJSON())
 	s.e.POST("/update/", s.UpdateJSON())
 	s.e.GET("/value/:type/:name", s.ShowMetric())
 	s.e.POST("/value/", s.ShowMetricJSON())
