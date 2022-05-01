77d7afe1b5152886d2fb9835e352fda9a80c4118
Siesta <siesta.cloud@gmail.com>
2022-05-01 11:58:19 +0300

    add vars restore store_file store_interval

--- internal/server/config/conf.go
+++ internal/server/config/conf.go
@@ -8,13 +8,13 @@ type ServerConfig struct {
 }
 
 type Server struct {
-	LogLevel  string `mapstructure:"log_level"`
-	LogFile   string `mapstructure:"log_file"`
-	Address   string `env:"ADDRESS" envDefault:"localhost:8080" mapstructure:"address"`
-	StoreFile string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json" mapstructure:"storefile"`
-	Restore   bool   `env:"RESTORE" envDefault:"true" mapstructure:"restore"`
-	SI        int    `env:"STORE_INTER"`
-	Timeout   struct {
+	LogLevel      string `mapstructure:"log_level"`
+	LogFile       string `mapstructure:"log_file"`
+	Address       string `env:"ADDRESS" envDefault:"localhost:8080" mapstructure:"address"`
+	StoreInterval int    `env:"STORE_INTERVAL" envDefault:"10"`
+	StoreFile     string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json" mapstructure:"storefile"`
+	Restore       bool   `env:"RESTORE" envDefault:"true" mapstructure:"restore"`
+	Timeout       struct {
 		// Server is the general server timeout to use
 		// for graceful shutdowns
 		Server time.Duration `mapstructure:"server"`
