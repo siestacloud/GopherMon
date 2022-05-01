722a2c78da423566551203649fd7bbbd4526c6b2
Siesta <siesta.cloud@gmail.com>
2022-05-01 08:59:34 +0300

    add vars only 3

--- internal/server/config/conf.go
+++ internal/server/config/conf.go
@@ -13,7 +13,7 @@ type Server struct {
 	Address  string `env:"ADDRESS" envDefault:"localhost:8080" mapstructure:"address"`
 	// StoreFile     string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json" mapstructure:"storefile"`
 	// Restore       bool   `env:"RESTORE" envDefault:"true" mapstructure:"restore"`
-	StoreInterval int64 `env:"STORE_INTERVAL" envDefault:"300" mapstructure:"storeinterval"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300" mapstructure:"storeinterval"`
 	Timeout       struct {
 		// Server is the general server timeout to use
 		// for graceful shutdowns
