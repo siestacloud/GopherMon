b93273e3d2c1e039dfb4537d214b561646f754ff
Siesta <siesta.cloud@gmail.com>
2022-05-04 13:23:14 +0300

    compress

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,20 +1,25 @@
 package server
 
 import (
+	"strings"
+
 	"github.com/labstack/echo/v4"
 )
 
 // Process is the middleware function.
 func (s *APIServer) ShowStatus(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		if err := next(c); err != nil {
-			c.Error(err)
+
+		if !strings.Contains(c.Request().Header.Get("Accept-Encoding"), "gzip") { //Заголовка нет
+			return next(c)
 		}
+
+		c.Response().Header().Add("Content-Encoding", "gzip")
 		// s.mutex.Lock()
 		// defer s.mutex.Unlock()
 		// s.RequestCount++
-		status := c.Request().Header
-		s.l.Info("Middleware: ", status)
-		return nil
+		h := c.Request().Header
+		s.l.Info("Middleware: request have Accept-Encoding header ", h)
+		return next(c)
 	}
 }
