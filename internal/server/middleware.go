99ae14c5554729f293fd49cfa6f62cc066b0a5e3
Siesta <siesta.cloud@gmail.com>
2022-05-04 13:42:13 +0300

    compress

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -10,16 +10,19 @@ import (
 func (s *APIServer) ShowStatus(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
-		if !strings.Contains(c.Request().Header.Get("Accept-Encoding"), "gzip") { //Заголовка нет
+		h := c.Request().Header
+		s.l.Info("Middleware: request have Accept-Encoding header ", h)
+
+		if !strings.Contains(c.Request().Header.Get("Accept-Encoding"), "gzip") {
+			//Заголовка Accept-Encoding нет
 			return next(c)
 		}
 
-		c.Response().Header().Add("Content-Encoding", "gzip")
+		// c.Response().Header().Add("Content-Encoding", "gzip")
 		// s.mutex.Lock()
 		// defer s.mutex.Unlock()
 		// s.RequestCount++
-		h := c.Request().Header
-		s.l.Info("Middleware: request have Accept-Encoding header ", h)
+
 		return next(c)
 	}
 }
