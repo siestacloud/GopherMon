3fb1f6339b04f57aa83bb544cb10019da8252086
Xader <kamojabishivili@gmail.com>
2022-05-19 10:12:58 +0300

    Исправил проблемы с запуском сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,32 @@
 package main
 
-import "github.com/MustCo/Mon_go/internal/server/apiserver"
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/MustCo/Mon_go/internal/server"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.json", "path to config json file")
+}
 
 func main() {
-	config := apiserver.Config{bind_addr: ":8080"}
-	server := apiserver.APIServer.New(config)
+	flag.Parse()
+	config := server.NewConfig()
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(data, config)
+
+	server := server.New(config)
 	server.Start()
 
 }
--- internal/server/apiserver.go
+++ internal/server/apiserver.go
@@ -1,12 +1,8 @@
-package apiserver
+package server
 
 import (
 	"net/http"
 )
-
-type Config struct {
-	bind_addr string
-}
 
 type APIServer struct {
 	config *Config
@@ -18,10 +14,9 @@ func New(config *Config) *APIServer {
 
 func UpdateMetrics(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	return
 }
 
 func (s *APIServer) Start() error {
 	http.HandleFunc("/update", UpdateMetrics)
-	return http.ListenAndServe(s.config.bind_addr, nil)
+	return http.ListenAndServe(s.config.BindAddr, nil)
 }
