9e174b570221146e5eeccc6efebddcd87576c80c
MustCo <kamojabishvili@gmail.com>
2022-06-18 20:17:50 +0300

    подправил тесты агента

--- internal/agent/apiagent_test.go
+++ internal/agent/apiagent_test.go
@@ -1,14 +1,16 @@
 package agent
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/MustCo/Mon_go/internal/utils"
 )
 
 func TestAPIAgent_SendMetric(t *testing.T) {
 	type args struct {
 		name string
-		m    reflect.Value
+		g    utils.Gauge
+		c    utils.Counter
 	}
 	tests := []struct {
 		name    string
@@ -16,15 +18,18 @@ func TestAPIAgent_SendMetric(t *testing.T) {
 		args    args
 		wantErr bool
 	}{{
-		name:    "Test1",
-		c:       &APIAgent{config: &Config{ReportAddr: "http://127.0.0.1:8080", ReportInterval: 10, PollInterval: 2}},
-		args:    args{name: "Mymetric", m: reflect.ValueOf(0)},
+		name:    "Test counter 1",
+		c:       &APIAgent{config: &Config{ReportAddr: "127.0.0.1:8080", ReportInterval: 10, PollInterval: 2}},
+		args:    args{name: "Mymetric", c: 0, g: 125.453},
 		wantErr: true,
 	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.c.sendMetric(tt.args.name, &tt.args.m); (err != nil) != tt.wantErr {
+			if err := tt.c.sendMetric(tt.args.name, &tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("APIAgent.SendMetric() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err := tt.c.sendMetric(tt.args.name, &tt.args.g); (err != nil) != tt.wantErr {
 				t.Errorf("APIAgent.SendMetric() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
