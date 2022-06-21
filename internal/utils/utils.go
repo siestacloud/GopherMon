506d8ff6df35748489ae744e35cd4b67882ed68a
MustCo <kamojabishvili@gmail.com>
2022-06-21 23:07:36 +0300

    переделал агент через новую структуру метрик

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -30,32 +30,47 @@ type Metrics struct {
 	Delta *int64   `json:"delta,omitempty"`
 	Value *float64 `json:"value,omitempty"`
 }
+
+func NewMetrics(id, mtype string) *Metrics {
+	return &Metrics{
+		ID:    id,
+		MType: mtype,
+		Value: new(float64),
+		Delta: new(int64),
+	}
+}
+
 type MetricsStorage map[string]Metrics
 
 func NewMetricsStorage() MetricsStorage {
-	m := MetricsStorage{}
-	return m
+	m := NewMetrics("PollCount", "counter")
+	return MetricsStorage{m.ID: *m}
 }
 
 func (m MetricsStorage) Poll() {
+	g := "gauge"
 	*m["PollCount"].Delta += 1
 	metrics := &runtime.MemStats{}
 	runtime.ReadMemStats(metrics)
 	mtrx := reflect.ValueOf(metrics).Elem()
 	for i := 0; i < mtrx.NumField(); i++ {
 		f := mtrx.Field(i)
+		m[mtrx.Type().Field(i).Name] = *NewMetrics(mtrx.Type().Field(i).Name, g)
 		switch f.Kind() {
-		case reflect.Uint64, reflect.Uint32:
-			*m[mtrx.Type().Field(i).Name].Delta = int64(f.Uint())
 		case reflect.Int32, reflect.Int64:
-			*m[mtrx.Type().Field(i).Name].Delta = int64(f.Int())
-		case reflect.Float32, reflect.Float64:
-			*m[mtrx.Type().Field(i).Name].Value = float64(f.Float())
+			*m[mtrx.Type().Field(i).Name].Value = float64(f.Int())
 
+		case reflect.Uint64, reflect.Uint32:
+			*m[mtrx.Type().Field(i).Name].Value = float64(f.Uint())
+
+		case reflect.Float32, reflect.Float64:
+			*m[mtrx.Type().Field(i).Name].Value = f.Float()
 		}
+
 	}
 	seed := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(seed)
+	m["RandomValue"] = *NewMetrics("RandomValue", g)
 	*m["RandomValue"].Value = r.Float64()
 	log.Println("Poll metrics")
 }
